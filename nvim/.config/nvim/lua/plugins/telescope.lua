aa809948b9a1dc8961d14bd202b6b378eabd1cc1
Daniel Gil <danielgil@mbp.local>
2025-12-19 19:33:33 +0100

    udpate comments from telecope config file

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,6 @@ return {
   config = function()
     local builtin = require("telescope.builtin")
     vim.keymap.set('n', '<C-t>', builtin.find_files, {}) -- fuzzy search to find files in our project
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) -- grep through our project
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) -- find  grep through our project
   end
 }
