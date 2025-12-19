2ce15f0f0b70ef5fb4a2f5caff350f28c00b2753
Daniel Gil <danielgil@mbp.local>
2025-12-19 19:32:47 +0100

    show in nvim relative line numbers

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -3,6 +3,9 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
